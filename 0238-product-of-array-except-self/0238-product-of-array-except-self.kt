49307f98ef3a7ca89c0fdcfaba2d0991af15e4d6
mshshshm <149753475+mshshshm@users.noreply.github.com>
2024-03-25 20:27:50 +0900

    Time: 308 ms (52.64%), Space: 50.5 MB (29.38%) - LeetHub

---
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun productExceptSelf(nums: IntArray): IntArray {
+        var result = IntArray(nums.size)
+        
+        var p = 1
+        for(i in nums.indices) {
+            result[i] = p;
+            p *= nums[i];
+        }
+        p=1
+        for(i in nums.indices.reversed()) {
+            result[i] *= p;
+            p *= nums[i];
+        }
+        return result;
+    }
+}
